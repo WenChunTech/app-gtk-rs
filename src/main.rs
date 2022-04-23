744b141e065dd5f5db9ac7c4147bdf528430efb9
FWC-WIN\fwc <1643806656@qq.com>
2022-04-23 17:43:32 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use std::cell::Cell;
+use std::rc::Rc;
+
+use gtk::glib::{clone, GString};
+use gtk::{prelude::*, Button};
+use gtk::{Application, ApplicationWindow};
+
+fn main() {
+    let app = Application::builder()
+        .application_id("org.gtk.rs.demo")
+        .build();
+
+    app.connect_activate(build_ui);
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    let btn = Button::builder()
+        .label("1")
+        .margin_bottom(12)
+        .margin_top(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+
+    let btn_increase = Button::builder()
+        .label("Increase")
+        .margin_bottom(12)
+        .margin_end(12)
+        .margin_start(12)
+        .margin_top(12)
+        .build();
+
+    let btn_decrease = Button::builder()
+        .label("Decrease")
+        .margin_bottom(12)
+        .margin_end(12)
+        .margin_start(12)
+        .margin_top(12)
+        .build();
+
+    let number = Rc::new(Cell::new(0));
+    let number_clone = number.clone();
+
+    btn_increase.connect_clicked(move |_| number.set(number.get() + 1));
+    btn_decrease.connect_clicked(move |_| number_clone.set(number_clone.get() - 1));
+
+    let gtk_box = gtk::Box::builder()
+        .orientation(gtk::Orientation::Horizontal)
+        .build();
+
+    gtk_box.append(&btn_increase);
+    gtk_box.append(&btn_decrease);
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("GTK")
+        .child(&gtk_box)
+        .build();
+
+    window.present();
+}
