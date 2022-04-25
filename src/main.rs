2d6433a79860f06e9ca8863413e7cd8dc7b1dc30
FWC-WIN\fwc <1643806656@qq.com>
2022-04-26 00:13:54 +0800

    update ci and demo

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::cell::Cell;
 use std::rc::Rc;
 
-use gtk::glib::{clone, GString};
-use gtk::{prelude::*, Button};
+use gtk::glib::clone;
+// use gtk::glib::{clone, GString};
+use gtk::glib;
+use gtk::prelude::*;
+use gtk::Button;
 use gtk::{Application, ApplicationWindow};
-
 fn main() {
     let app = Application::builder()
         .application_id("org.gtk.rs.demo")
@@ -32,10 +34,16 @@ fn build_ui(app: &Application) {
         .build();
 
     let number = Rc::new(Cell::new(0));
-    let number_clone = number.clone();
+    // let number_clone = number.clone();
 
-    btn_increase.connect_clicked(move |_| number.set(number.get() + 1));
-    btn_decrease.connect_clicked(move |_| number_clone.set(number_clone.get() - 1));
+    // btn_increase.connect_clicked(move |_| number.set(number.get() + 1));
+    // btn_decrease.connect_clicked(move |_| number_clone.set(number_clone.get() - 1));
+    btn_increase.connect_clicked(
+        clone!(@weak number, @weak btn_decrease => move |_| {number.set(number.get() + 1);  btn_decrease.set_label(&number.get().to_string())}),
+    );
+
+    btn_decrease.connect_clicked(clone!(@weak btn_increase => move |_| number.set(number.get() - 1); btn_increase.set_label(&number.get().to_string())));
+    // btn_decrease.connect_clicked(move |_| number_clone.set(number_clone.get() - 1));
 
     let gtk_box = gtk::Box::builder()
         .orientation(gtk::Orientation::Horizontal)
